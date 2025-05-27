ff1390a49d42f4de315640dbc034e4fcbc16bb65
Fherrera-ibanez <83441484+Fherrera-ibanez@users.noreply.github.com>
2025-05-27 23:29:03 +0000

    Deploying to gh-pages from @ Fherrera-ibanez/Fherrera-ibanez.github.io@b9365509d36771713f4a8b84cdee2a7c99db12f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -382,17 +382,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/images/";
           
         },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
-          
-        },
       },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
